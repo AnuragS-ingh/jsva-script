9becfe2756ea40c14493126fa3edb7b294c9aa19
AnuragS-ingh <168142800+AnuragS-ingh@users.noreply.github.com>
2024-04-29 21:22:11 +0000

    finished the varable topic and the basics of the variable...

---
+++ codeWithChai/variables.js
@@ -0,0 +1,50 @@
+// variables is the name of the memory space where the data is stored i.e it is the name of the memory location where the data is stored..
+
+// In the latest version of the javaScript we have three different menthods for declearing the variable..
+
+// There are three different keywords that is used to decleare the varable in the javaScript:-
+/*
+first is Declearing the variable using the "Const keyword":-
+
+->The variable that is decleared using the const keyword cannot be changed throughout the code.Once it is decleared and assigned the value it 
+is permenently assigned to that variable we cannot change the value of that variable throughout the code.
+
+Now let's see some example of the decleration of the variable using the const:-
+*/
+
+const pi=3.14;  // Now the value of this variable cannot be changed because it is decleared using the const. We cannot assign another
+// value to this variable
+
+
+/*
+Now second keyword we have is "var keyword":-
+This is the another keyword that is used to decleare the variable. now a days var is not prefferd to use 
+because of the issues in the block scope and the functional scope. For example:-
+
+var nameOfTheStudent="Anurag singh";
+
+The value of the above varable can be changed because it is decleared using the var keyword.
+*/
+
+/*
+The third method that we have for declearing the variable is by using the "let keyword". This is the most preferable method 
+for declearing variable. Let's see some example:-
+
+let name="Anurag singh";
+The value of this varable also can be changed.
+*/
+
+var Name="anurag singh";
+let lastName="singh";
+city="Barathawa";
+
+// pi=4.25;//this is not allowed
+Name="shitesh"
+city="delhi";
+
+
+console.table([pi,Name,lastName,city]);//This is the another method of using the console for printing the value of the multiple 
+// variables is the tabuler form 
+
+// Note: if we not assign any value to the variable then the js will return us undefined.****************
+
