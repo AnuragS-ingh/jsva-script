4cce10ee8d3886b690df30736117e77a62eddd9f
AnuragS-ingh <168142800+AnuragS-ingh@users.noreply.github.com>
2024-05-04 18:43:59 +0000

    studying the string and its methods

---
+++ codeWithChai/Memory.js
@@ -0,0 +1,20 @@
+/*There are two types of the memory in the javaScript. first is stack memeory and the second is Heap memory
+
+All the primitive data types uses the stack memory.when ever we uses the stack memory then we get the copy.
+
+=> All the non-primitive data types uses the Heap memory. When ever the Heap memory is used we get the refrence i.e all the changes 
+that are done is changed in the original variable object or array.
+
+*/
+
+// let student = {
+//     Name: "Anurag singh",
+//     class: "CSE",
+//     Roll : 66
+// };
+
+// let another = student;
+
+// another.Name="shitesh"
+// console.log(another);
+// console.log(student)
---
+++ codeWithChai/String.js
@@ -0,0 +1,20 @@
+/*
+
+String in the JavaScript:-
+
+=> String is the sequence of the character. In the JavaScript the string is enclosed in both single (' ') and double(" ") quote.
+for example:-
+
+let Name = "Anurag singh"
+
+let lastName = 'singh';
+
+There is another way to decleare string :-
+
+let New = new String("Anurag singh");  => Here the concept of the constructor is used.. In this code the string constructor is
+used.
+
+console.log(New)
+
+*/
+
